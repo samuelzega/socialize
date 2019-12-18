98e17af35c4ddf2c192ed8041de76c4501adfc7f
Harfi <harfialpharaby@gmail.com>
2019-12-18 14:03:31 +0700

    progress upload image

--- controllers/FeedController.js
+++ controllers/FeedController.js
@@ -1,4 +1,7 @@
+'use strict'
+
 const {Feed, Tag, FeedTags} = require('../models');
+const fs = require('fs');
 
 class FeedController {
     static showFeed(req, res) {
@@ -82,6 +85,31 @@ class FeedController {
             }).catch((err) => {
                 res.send(err);
             });
+    }
+
+    static uploadImage(req, res) {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.png");
+
+        if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(200)
+                    .contentType("text/plain")
+                    .end("File uploaded!");
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
     }
 }
 
--- routes/feed.js
+++ routes/feed.js
@@ -9,6 +9,4 @@ router.get('/', FeedController.showFeed);
 router.get('/add', FeedController.showAddFeedForm);
 router.post('/add', FeedController.addFeed);
 router.get('/edit/:id', FeedController.showEditFeedForm);
-// router.post('/add', FeedController.editFeed);
-
 module.exports = router
